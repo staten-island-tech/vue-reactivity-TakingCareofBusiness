fb4a6ca2db0e2df5abb72a71de6c763a10451f0d
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-01-31 12:30:05 -0500

    Fish background

---
+++ app/src/components/StartHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <title>At Home Aquascape</title>
+    <RouterLink>Start Game</RouterLink>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
