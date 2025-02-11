47a003a9ac1f736322ac514c7ce950748e52c3b2
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-02-11 12:32:43 -0500

    Fish

---
+++ app/src/decor.js
@@ -0,0 +1,18 @@
+const fishList = [
+  { name: 'Purple Pristella Tetra Fish', image: '/PurplePristellaTetraFish.png' },
+  { name: 'Green Pristella Tetra Fish', image: '/GreenPristellaTetraFish.png' },
+  { name: 'Orange Pristella Tetra Fish', image: '/OrangePristellaTetraFish.png' },
+  { name: 'Platinum Male Betta Fish', image: '/PlatinumMaleBettaFish.png' },
+  { name: 'Black Orchid Male Betta Fish', image: '/BlackOrchidMaleBettaFish.png' },
+  { name: 'Starfire Red Tiger Barb Fish', image: '/StarfireRedTigerBarbFish.png' },
+  { name: 'Butterfly Male Betta Fish', image: '/ButterflyMaleBettaFish.png' },
+  { name: 'Rose Petal Male Betta Fish', image: '/RosePetalMaleBettaFish.png' },
+  { name: 'Female Halfmoon Betta Fish', image: '/FemaleHalfmoonBettaFish.png' },
+  { name: 'Electric Green Barb Fish', image: '/ElectricGreenBarbFish.png' },
+  { name: 'Electric Blue Acara Cichlid Fish', image: '/ElectricBlueAcaraCichlidFish.png' },
+  { name: 'Dumbo Halfmoon Male Betta Fish', image: '/DumboHalfmoonMaleBettaFish.png' },
+  { name: 'Crowntail Female Betta Fish', image: '/CrowntailFemaleBettaFish.png' },
+  { name: 'Twintail Halfmoon Male Betta Fish', image: '/TwintailHalfmoonMaleBettaFish.png' },
+  { name: 'Halfmoon Male Betta Fish', image: '/HalfmoonMaleBettaFish.png' },
+  { name: 'Dragon Scale Male Betta Fish', image: '/DragonScaleMaleBettaFish.png' },
+]
