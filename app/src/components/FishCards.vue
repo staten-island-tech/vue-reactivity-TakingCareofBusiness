8a75485656407766fabd1cf69e451b2b4581b0cd
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-02-12 23:14:53 -0500

    Evil red code

--- app/src/components/FishCards.vue
+++ app/src/components/FishCards.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="w-1/4 h-1/5"></div>
+  <div class="w-1/4 h-1/5 bg-white">
+    <h1>{{ item.name }}</h1>
+    <img class="w-2/3 h-1/2" :src="item.image" alt="Fish image" />
+    <slot></slot>
+  </div>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<script setup>
+defineProps({ item: Object })
+</script>
--- app/src/views/FishView.vue
+++ app/src/views/FishView.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <FishCards v-for="item in fishList" :key="item.name" :item="item><button @click="addToTank(item)">Click Me!</button>
+      </FishCards>
+  </div>
 </template>
 
 <script setup>
 import { fishList } from '@/fish.js'
+import FishCards from '../components/FishCards.vue'
+const occupantList = reactive({})
+function addToTank(data) {
+  console.log(data)
+}
 </script>
 
 <style scoped></style>
