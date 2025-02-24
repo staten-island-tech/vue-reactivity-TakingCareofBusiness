c9fd5df06be3f979bfe2e60d59cc9b80b00bd5b9
TakingCareofBusiness <superspongysquid@yahoo.com>
2025-02-24 04:28:32 -0500

    huh

--- app/src/views/FishView.vue
+++ app/src/views/FishView.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <div class="bg-[url('/tank.png')] w-1/3 h-1/5">
-      <TankedFish v-for="item in screenOccupants" :key="item.name" :item="item" />
+    <div
+      class="relative bg-[url('/tank.png')] w-1/2 h-1/4 bg-no-repeat bg-cover"
+      :style="{
+        backgroundImage: 'url(/tank.png)', // Dynamically set the background image
+        backgroundSize: 'cover', // Ensure the background image covers the entire div
+        backgroundPosition: 'center', // Center the background image
+      }"
+    >
+      <TankedFish
+        v-for="(item, index) in screenOccupants"
+        :key="index"
+        :item="item"
+        :style="getPositionStyle(item)"
+      />
     </div>
     <div>
-      <FishCards v-for="item in fishList" :key="item.name" :item="item"
-        ><button @click="addToTank(item)">Click Me!</button>
+      <FishCards v-for="item in fishList" :key="item.name" :item="item">
+        <button @click="addToTank(item)">Click Me!</button>
       </FishCards>
     </div>
   </div>
@@ -15,11 +27,35 @@
 import { fishList } from '@/fish.js'
 import FishCards from '../components/FishCards.vue'
 import { ref, reactive } from 'vue'
+import TankedFish from '../components/TankedFish.vue'
+
 const screenOccupants = reactive([])
+
 function addToTank(data) {
-  screenOccupants.push(data)
+  const newFish = {
+    ...data,
+    position: {
+      top: `${Math.random() * 100}%`,
+      left: `${Math.random() * 100}%`,
+    },
+    size: Math.floor(Math.random() * 50) + 30,
+  }
+  screenOccupants.push(newFish)
   console.log(screenOccupants)
+}
+function getPositionStyle(item) {
+  return {
+    position: 'absolute',
+    top: item.position.top,
+    left: item.position.left,
+    width: `${item.size}px`,
+    height: `${item.size}px`,
+  }
 }
 </script>
 
-<style scoped></style>
+<style>
+div {
+  display: block;
+}
+</style>
